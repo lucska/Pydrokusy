0addcf72411edb8e2940a208c25c35480b711a4a
Lucie Zemanova <lucska@gmail.com>
2017-09-22 15:10:06 +0200

    Functional version with defined function, problem with overlapping motifs

--- protein_motif_test2.py
+++ protein_motif_test2.py
@@ -8,6 +8,7 @@ def read_input(input_file):
         fasta_list.append(items.strip()) #je nutné jednotlivé položky orezat jinak tak zůstava \n a nefunguje to dale
     return fasta_list
 
+
 def download_of_data(list_of_sequences):
     """download sequences form Uniprot database according to uniprot ID in input file"""
     fasta_record_list = []
@@ -20,29 +21,43 @@ def download_of_data(list_of_sequences):
     return fasta_record_list
 
 def protein_sequence_from_fasta (fasta_sequence_list):
-    """select protein sequence from fasta format"""
+
     import re
+    sequences_only = []
     for i in fasta_sequence_list:
-        sequences_reg = re.compile ("M[A-Z]{10,}")#davam 10x aby nebylo nebezpeci, že se tam objeví něco z header (třeba z HUMAN)
-        sequences_only_search = sequences_reg.search (i)
-        sequences_only= sequences_only_search.group()
-        return sequences_only
+        sequences_reg = re.compile ("[A-Z]{10,}")#davam 10x aby nebylo nebezpeci, že se tam objeví něco z header (třeba z HUMAN)
+        sequences_only_search = sequences_reg.search(i).group()
+        sequences_only.append(sequences_only_search)
+    return sequences_only #dostanu seznam se vsema sekvencema
+
 
 def finding_glycosylation_motif (protein_sequence):
     """find glycosylation motif in protein sequence"""
     import re
-    list_of_position = []
-    gly_motif = re.compile("N[^P][S|T][^P]")
+    list_position = []
+    gly_motif = re.compile("N[^P][S|T][^P]") #je tu problem, že pokud se ty motivy prekryvají tak je nevidim, naprř. pozice 115 a 116
     motif_in_protein_search = gly_motif.finditer(protein_sequence)
     for match in motif_in_protein_search:
-        list_of_position.append (match.start()+1)
-    return list_of_position
+        position = match.start()+1
+        list_position.append (position)
+
+    return list_position
 
 
-list_of_fasta = read_input("protein_test.txt")
-print(list_of_fasta)
+list_of_fasta = read_input("protein_test2.txt")
+
 sequence_fasta_list = download_of_data(list_of_fasta)
-sequence_only = protein_sequence_from_fasta(sequence_fasta_list)
-print (sequence_only) #nevypíše mi to všechny sekvence, takze pak ani nedopocita pro všechny....jak na to????
-glycosylation = finding_glycosylation_motif (sequence_only)
-print(glycosylation)
+
+sequence = protein_sequence_from_fasta(sequence_fasta_list)
+glycosylation = []
+
+for i in sequence:
+    glycos = finding_glycosylation_motif (i)
+    glycosylation.append(glycos)
+
+results = dict(zip(list_of_fasta,glycosylation))
+
+for keys,values in results.items():
+    if values != []:
+        values_change = " ".join(str(i).strip() for i in values)
+        print (keys, "\n",values_change) #zaver nema uplne správny formát!!!
